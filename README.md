71eec4033f14b37b6434555342431213fcd9ea65
Bruno Frasan <brunofrancco@gmail.com>
2024-12-30 16:10:32 +0000

    done

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # planets_projet-node_i-o
+
+# lets explore planets that we might live using node.js and csv-parse
+
+# this will be achieved using data from kepler at
+
+# https://exoplanetarchive.ipac.caltech.edu/cgi-bin/TblView/nph-tblView?app=ExoTbls&config=cumulative
+
+# and theres an article where we can see the elegibility of the planet to be habitable or not
+
+# https://www.centauri-dreams.org/2015/01/30/a-review-of-the-best-habitable-planet-candidates/
---
+++ index.js
@@ -0,0 +1,53 @@
+import { parse } from 'csv-parse'
+import fs from 'fs'
+
+const koi = []
+
+function isHabitable() {
+	let planets = []
+	koi.forEach((planet) =>
+		planet.koi_disposition === 'CONFIRMED' &&
+		planet.koi_insol > 0.36 &&
+		planet.koi_insol < 1.11 &&
+		planet.koi_prad < 1.6
+			? (planets = [
+					...planets,
+					{
+						candidate: planet.koi_disposition,
+						name: planet.kepoi_name,
+						insulation_flux: planet.koi_insol,
+						planetary_radius: planet.koi_prad,
+						period: planet.koi_period,
+						transition: planet.koi_duration,
+					},
+			  ])
+			: ''
+	)
+
+	return planets
+}
+
+let reader = fs.createReadStream('kepler_data.csv')
+reader
+	.pipe(
+		parse({
+			comment: '#',
+			columns: true,
+		})
+	)
+	.on('data', (data) => {
+		koi.push(data)
+	})
+	.on('error', (err) => {
+		console.log(err)
+	})
+	.on('end', () => {
+		// console.log(koi)
+		// console.log('done')
+		console.log(isHabitable())
+		console.log(
+			`There is aprox ${isHabitable().length} in ${
+				koi.length
+			} planets using a basic criteria that coulb be habitable by humans!!!❤️`
+		)
+	})
